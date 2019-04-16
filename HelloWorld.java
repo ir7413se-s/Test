17bb7fb8fc128be8b4e10322c954fe0af0e64913
ir7413se-s <ir7413se-s@student.lu.se>
2019-04-16 23:39:53 +0200

    Add new sentence

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
-//HelloWorld in Java
+//HelloWorld in Java 
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("How are you?");
     }
 
 }
